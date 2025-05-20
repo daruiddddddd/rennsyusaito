2d6ab1f90b4d78ae7a342ce0a491835899668d31
daruiddddddd <ryu08ma@gmail.com>
2025-05-20 11:12:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,20 @@ const io = new Server(server);
 let shapes = [];
 let imageRotation = 0;
 
+// 画像データ（base64）を保持
+let currentImageData = null;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    // 初期描画情報と画像を送る
     socket.emit('initialDraw', shapes);
     socket.emit('rotateImage', imageRotation);
+    if (currentImageData) {
+        socket.emit('image', currentImageData);
+    }
 
     socket.on('draw', (newShapes) => {
         shapes = newShapes;
@@ -29,6 +37,14 @@ io.on('connection', (socket) => {
 
     socket.on('getInitialDraw', () => {
         socket.emit('initialDraw', shapes);
+        if (currentImageData) {
+            socket.emit('image', currentImageData);
+        }
+    });
+
+    socket.on('image', (imgDataUrl) => {
+        currentImageData = imgDataUrl;
+        socket.broadcast.emit('image', imgDataUrl);
     });
 });
 
